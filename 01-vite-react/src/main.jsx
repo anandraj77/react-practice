57f7154bd5d5d6f80d42949e1f7a90071bca3152
Anand Raj <anandr1213@gmail.com>
2024-09-16 12:33:08 +0530

    hooks and react

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// THIS DOESN'T WORK
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google !</a>
+)
+
+const anotherUser = "chai aur react"
+
+// WILL HAVE TO USE THIS
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  ReactElement
+  // <App />
 )
+ 
