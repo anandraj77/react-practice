d517e1637d47e9a2bcda4974bb70050d2c2e2901
Anand Raj <anandr1213@gmail.com>
2024-10-15 22:35:30 +0530

    env files added

--- Thapa Technical/react_router_v6/src/api/GetAPIData.jsx
+++ Thapa Technical/react_router_v6/src/api/GetAPIData.jsx
@@ -2,7 +2,7 @@
 
 export const getMoviesData = async() => {
     try {
-      const response = await fetch("https://www.omdbapi.com/?i=tt38961988&apikey=1c12799f&s=titanic&page=1")
+      const response = await fetch(`https://www.omdbapi.com/?i=tt38961988&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`)
       const data = response.json();
       return data
     } catch (error) {
