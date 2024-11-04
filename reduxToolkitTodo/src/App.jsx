eeb75a84e511e358c12990841790489f42906e3c
Anand Raj <anandr1213@gmail.com>
2024-11-05 02:30:52 +0530

    redux-toolkit-HC

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
